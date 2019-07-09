442956e483995bedbf7e9134bb947e51eb124fae
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-07-09 23:19:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,25 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    # @recipe.name = params[:name]
+    # @recipe.ingredients = params[:ingredients]
+    # @recipe.cook_time = params[:cook_time]
+    # @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+
 end
